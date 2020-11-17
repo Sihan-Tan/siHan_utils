0e924583956f0d10c38f2c34149ee2a83b677e79
谭璇 <tanxuan_2013@163com>
2020-11-17 08:05:45 +0800

    change:nyc config

--- nyc.config.js
+++ nyc.config.js
@@ -3,22 +3,20 @@
  * @Date: 2020-10-29 10:51:00
  * @LastEditTime: 2020-10-30 17:18:13
  * @LastEditors: Tan Xuan
- * @Description: 
+ * @Description:
  */
 module.exports = {
-    // 'check-coverage': true,
-    // 'skip-full': true,
-    'all': true,
-    'reporter': ['html', 'text'],
-    'include': [
-      'src/**/*.js'
-    ],
-    'exclude': [
-      'src/modules/scroll.js',
-      'src/modules/wechat.js',
-      'src/modules/cookie.js',
-      'src/config/*.js',
-      'src/index.js'
-    ],
-    'report-dir': './coverage'
-  };
+  // 'check-coverage': true,
+  // 'skip-full': true,
+  all: true,
+  reporter: ["html", "text"],
+  include: ["src/**/*.js"],
+  exclude: [
+    // 'src/modules/scroll.js',
+    // 'src/modules/wechat.js',
+    // 'src/modules/cookie.js',
+    "src/config/*.js",
+    "src/index.js",
+  ],
+  "report-dir": "./coverage",
+};
